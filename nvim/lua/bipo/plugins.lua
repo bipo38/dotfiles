111d191f19f31dd9221dec41608dad7dd8649270
bipo38 <bipoben7@gmail.com>
2023-10-03 16:32:05 +0000

    feat: add comments plugin

--- nvim/lua/bipo/plugins.lua
+++ nvim/lua/bipo/plugins.lua
@@ -43,5 +43,20 @@ return {
 			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 			"MunifTanjim/nui.nvim",
 		}
-	}
+	},
+	{ 'github/copilot.vim' },
+	{
+		'numToStr/Comment.nvim',
+		config = function()
+			require('Comment').setup({
+				pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+			})
+		end,
+		dependencies = {
+			'JoosepAlviste/nvim-ts-context-commentstring',
+			-- Allows commenting in multiple languages in the same file (e.g. HTML and JS in a .vue file)
+		},
+	},
+
+
 }
